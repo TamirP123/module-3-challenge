55725c56fb76cde859cbecfe3fae2790bacd0aee
Tamir <tamirphillips@live.com>
2023-09-20 11:59:00 -0400

    Built logic for the generator to work. Few bugs with length of password.

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-var length = 0;
-
+var length;
+var password = "";
+var builtPassword = "";
+var numbers = "1234567890"
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var upperCase = lowerCase.toUpperCase();
+var specialCharacters = "!#$%&*@_"
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -24,9 +29,44 @@ function generatePassword() {
     if(lengthChoice < 8 || lengthChoice > 128 || isNaN(lengthChoice)) {
         alert("Please choose a NUMBER that is at least 8 characters and no more than 128 characters.");
     }
-    else {
-        
-    }
+
+var lowerCaseChoice = confirm("Would you like your password to contain lower case letters?");
+
+if (lowerCaseChoice) {
+  builtPassword += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+}
+
+var upperCaseChoice = confirm("Would you like your password to contain upper case letters?");
+
+if (upperCaseChoice) {
+  builtPassword += upperCase[Math.floor(Math.random() * upperCase.length)];
+}
+
+var numbersChoice = confirm("Would you like your password to contain numbers?");
+
+
+if (numbersChoice) {
+  builtPassword += numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+var charactersChoice = confirm("Would you like your password to contain special characters?");
+
+
+if (charactersChoice) {
+  builtPassword += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+}
+
+for (var i = 0; i < length; i++) {
+  var randomInput = Math.floor(Math.random() * password.length);
+  password += builtPassword;
+}
+
+alert("Your password is: " + builtPassword);
+
+var newPassword = confirm("DO you want a new password?");
+if (newPassword) {
+    generatePassword();
+}
 }
 
 generatePassword();
